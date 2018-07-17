673c287fd1c225abd4ec81a0a614b2a897969467
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2018-07-17 12:42:17 +0530

    createMessage and newMessage

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-//PORT Number
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname , '../public');
-console.log(publicPath);
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-//Set MiddileWare
 app.use(express.static(publicPath));
 
-app.listen(port , () => {
-    console.log(`Server running on port ${port}`);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  //My custom event
+  socket.emit('newMessage' ,{
+      from: 'lenovo@yoga360.com',
+      text: 'Hello Dear User',
+      createdAt: '10:58',
+  });
+
+  //Client to Server(Bidirectional)
+    socket.on('createMessage', (newMessage) => {
+      console.log('New message landed ', newMessage);
+      io.emit('newMessage', {
+          from: newMessage.from,
+          to: newMessage.to,
+          date: new Date().getDate(),
+      });
+    });
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
